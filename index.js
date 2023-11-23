029008fbda5e955fabc2f7cef0626c61b9320748
ClaudineiAn <claudinei2092016@gmail.com>
2023-11-23 03:18:45 -0300

    Tests

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ var emailUser = ''
 
 const server = http.createServer((req, res) => {
 cors()(req, res, () => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     const chunks = [];
 
