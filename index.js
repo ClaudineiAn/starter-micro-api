a7167224bab2b80023fa8495a80116ea238406fa
ClaudineiAn <claudinei2092016@gmail.com>
2023-11-23 03:23:37 -0300

    Tests

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ var emailUser = ''
 
 const server = http.createServer((req, res) => {
 cors()(req, res, () => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
