361a1e94fdd2584a67484ba2a1fd3d7e4378d2ed
ClaudineiAn <claudinei2092016@gmail.com>
2023-12-02 19:47:36 -0300

    Test

--- index.js
+++ index.js
@@ -214,39 +214,31 @@ async function handleFileUpload(req, res) {
             const data1 = Buffer.concat(chunks);
             const data = Buffer.from(data1, 'base64');
 
-            const contentDisposition = req.headers['content-disposition'];
-            const match = contentDisposition && contentDisposition.match(/filename="(.+)"\r\n/);
-console.log(219)
-            if (match) {
-                console.log(221)
-                const originalFilename = match[1];
+            const originalFilename = match[1];
 
-                const allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
-                const fileExtension = path.extname(originalFilename).toLowerCase();
+            const allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
+            const fileExtension = path.extname(originalFilename).toLowerCase();
 
-                if (!allowedExtensions.includes(fileExtension)) {
-                    await handleUploadError(res, 'File extension is not allowed.');
-                    return;
-                }
-                
-                if (dataSize > 10485760) {
-                    await handleUploadError(res, 'File size exceeds the limit.');
-                    return;
-                }
-
-console.log(237)
-                const timestampedFilename = `${Date.now()}_${originalFilename}`;
-                const { updateProfilePicture } = require("./controller/users");
-                await updateProfilePicture({
-                    imagem_perfil_data: data,
-                    imagem_perfil_name: timestampedFilename,
-                    imagem_perfil_type: getContentType(fileExtension),
-                });
-    
-console.log(246)
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(timestampedFilename);
+            if (!allowedExtensions.includes(fileExtension)) {
+                await handleUploadError(res, 'File extension is not allowed.');
+                return;
             }
+            
+            if (dataSize > 10485760) {
+                await handleUploadError(res, 'File size exceeds the limit.');
+                return;
+            }
+
+            const timestampedFilename = `${Date.now()}_${originalFilename}`;
+            const { updateProfilePicture } = require("./controller/users");
+            await updateProfilePicture({
+                imagem_perfil_data: data,
+                imagem_perfil_name: timestampedFilename,
+                imagem_perfil_type: getContentType(fileExtension),
+            });
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(timestampedFilename);
         });
     } catch (error) {
         console.error('Error handling file upload:', error);
