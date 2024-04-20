aa91baaeb78505c5cbf74a24dc9914577a98fdb6
ClaudineiAn <claudinei2092016@gmail.com>
2024-04-19 21:58:01 -0300

    Tests

--- model/productsDAO.js
+++ model/productsDAO.js
@@ -1,6 +1,6 @@
 class productsDAO {
     static async home(db){
-        const sql = `(SELECT co.current, CONCAT("{",GROUP_CONCAT(CONCAT('[{"name":"',f.nome,'","price":',pf.preco,',"discount":',pf.desconto,"}]") SEPARATOR "','"),"}") "formats", pt.name "productType", p.nome, "0" AS "descounts",SUM(i.qtde) "TotalVendas", p.descricao, p.imagem, p.score, p.por, p.publicado, p.idade_de_leitura, p.numero_de_paginas, p.idioma, p.dimensoes, p.editora, p.edicao, p.tamanho_do_arquivo, p.countryid
+        const sql = `(SELECT co.current, CONCAT("[",GROUP_CONCAT(CONCAT('[{"name":"',f.nome,'","price":',pf.preco,',"discount":',pf.desconto,"}]") SEPARATOR ","),"]") "formats", pt.name "productType", p.nome, "0" AS "descounts",SUM(i.qtde) "TotalVendas", p.descricao, p.imagem, p.score, p.por, p.publicado, p.idade_de_leitura, p.numero_de_paginas, p.idioma, p.dimensoes, p.editora, p.edicao, p.tamanho_do_arquivo, p.countryid
         FROM venda v 
         INNER JOIN itens i ON (i.venda_idvenda=v.idvenda)
         INNER JOIN produto p ON (p.idproduto=i.produto_idproduto)
@@ -12,7 +12,7 @@ class productsDAO {
         GROUP BY p.idproduto
         LIMIT 50)
         UNION
-        (SELECT co.current, CONCAT("{",GROUP_CONCAT(CONCAT('[{"name":"',f.nome,'","price":',pf.preco,',"discount":',pf.desconto,"}]") SEPARATOR "','"),"}") "formats", pt.name "productType", p.nome, d.descounts, "0" AS "TotalVendas", p.descricao, p.imagem, p.score, p.por, p.publicado, p.idade_de_leitura, p.numero_de_paginas, p.idioma, p.dimensoes, p.editora, p.edicao, p.tamanho_do_arquivo, p.countryid
+        (SELECT co.current, CONCAT("[",GROUP_CONCAT(CONCAT('[{"name":"',f.nome,'","price":',pf.preco,',"discount":',pf.desconto,"}]") SEPARATOR ","),"]") "formats", pt.name "productType", p.nome, d.descounts, "0" AS "TotalVendas", p.descricao, p.imagem, p.score, p.por, p.publicado, p.idade_de_leitura, p.numero_de_paginas, p.idioma, p.dimensoes, p.editora, p.edicao, p.tamanho_do_arquivo, p.countryid
         FROM produto p
         JOIN (
             SELECT idproduto,MAX(desconto) "descounts"
@@ -23,8 +23,7 @@ class productsDAO {
         INNER JOIN produtotipo pt ON (pt.idproduto_tipo = p.idproduto_tipo)
         INNER JOIN produto_formato pf ON (pf.idproduto = p.idproduto)
         INNER JOIN formato f ON (f.idformato = pf.idformato)
-        WHERE co.nome = ? AND p.status = ?
-        WHERE d.descounts>0
+        WHERE co.nome = ? AND p.status = ? AND d.descounts>0
         GROUP BY d.idproduto
         LIMIT 50)
         ORDER BY RAND()`;
